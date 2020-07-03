ca7f4c923f70ebc4c13435aea26320217aacdc8e
Anupam <onlyprogramming000@gmail.com>
2020-07-04 01:05:41 +0530

    Integrating Weather Stack API

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-console.log("Starting");
+const request = require("request");
 
-setTimeout(() => {
-  console.log("delayed 2 secs");
-}, 2000);
+const URL =
+  "http://api.weatherstack.com/current?access_key=9431f7f828b921327ebdbb126217e5e3&query=20.593683,78.962883";
 
-console.log("Stopping");
+request({ url: URL }, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.current);
+});
