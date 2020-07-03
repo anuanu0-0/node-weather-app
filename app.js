295b36cf0cb12537af2ddf9f2eaa29921cce14b5
Anupam <onlyprogramming000@gmail.com>
2020-07-04 01:23:07 +0530

    Updating request options object

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const request = require("request");
 
-const URL =
+const url =
   "http://api.weatherstack.com/current?access_key=9431f7f828b921327ebdbb126217e5e3&query=20.593683,78.962883";
 
-request({ url: URL }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  console.log(response.body.current);
 });
